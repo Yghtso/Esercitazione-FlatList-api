a491e9b6faf822cfd64ec3afc0cd178ed37620af
Cristian Soare <soare.cristian@outlook.com>
2025-06-02 14:08:22 +0200

    using the custom hook

--- App.js
+++ App.js
@@ -3,17 +3,18 @@ import { ScrollView, RefreshControl, Text, SafeAreaView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import BookList from './components/BookList';
 import AddBook from './components/AddBook';
+import { useBooks } from './hooks/useBooks';
 
 export default function App() {
-  const [refreshing, setRefreshing] = useState(false);
 
-  const onRefresh = () => {
-    setRefreshing(true);
-    loadBooks().then(() => setRefreshing(false));
+  const { books, loading, error, addBook, deleteBook, fetchBooks } = useBooks();
+  
+  const onRefresh = async () => {
+    fetchBooks();
   };
 
-  useEffect(() => {
-    loadBooks();
+  useEffect(async () => {
+    fetchBooks();
   }, []);
 
   return (
